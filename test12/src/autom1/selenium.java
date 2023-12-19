2a855ef398e782fcf24b08879f236b7d5b1c9ae3
iram <iram@iram-Inspiron-3470>
2023-12-19 12:35:32 +0530

    Added new output message.

--- test12/src/autom1/selenium.java
+++ test12/src/autom1/selenium.java
@@ -3,7 +3,8 @@ package autom1;
 public class selenium {
 
 	public static void main(String[] args) {
-		System.out.print("i am shashi");
+		System.out.println("i am shashi");
+		System.out.println("i put of at sarjapur");
 	}
 
 }
